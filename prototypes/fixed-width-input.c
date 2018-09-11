faa5810d415dd667248d0b645f5f4132e3d1d249
Dylan Littler <dylan.littler@tutanota.com>
2018-09-11 18:28:31 +1000

    Fixed bugs in check_status() function of test.sh script.

--- prototypes/fixed-width-input.c
+++ prototypes/fixed-width-input.c
@@ -4,11 +4,13 @@
 
 /* This program will receive long strings of input and
  * insert newlines at the nearest space before the
- * line length limit. If line length is exceeded, input will be trimmed,
+ * line length limit, which is designed to reflect a length
+ * That will fit in a standard terminal.
+ * If maximum input length is exceeded, input will be trimmed,
  * and the user will be notified.
  */
 
-#define MAX_LINE_LENGTH 50
+#define MAX_LINE_LENGTH 50 // most terminal screens are longer than this
 #define MAX_INPUT 200
 
 int find_space(char *input, int start, int end) {
@@ -23,20 +25,23 @@ int find_space(char *input, int start, int end) {
 }
 
 char *insert_newlines(char *input) {
+  /* A newline will be inserted at a space at intervals as close to
+   * MAX_LINE_LENGTH as possible. Iteration will continue as long
+   * as the remaining string exceeds this limit.
+   */
+
   int line_end = MAX_LINE_LENGTH;
   int line_start = 0;
   int prev_line_end = 0;
 
   for (line_start = 0; line_start <= strlen(input); line_start += MAX_LINE_LENGTH) {
     if (strlen(input) - line_start <= MAX_LINE_LENGTH) {
-      return input;
+      return input; // break the loop if inserting newline is not needed
     } else {
       line_end = find_space(input, prev_line_end, line_end);
       input[line_end] = '\n';
-      prev_line_end = line_end;
+      prev_line_end = line_end; // save previous end point to be used as starting point for find_space()
       line_end += MAX_LINE_LENGTH;
-      printf("Line_start: %d, line_end: %d, prev_line_end: %d\n",
-	     line_start, line_end, prev_line_end);
     }
   }
   return input;
@@ -48,7 +53,12 @@ int main(int argc, char *argv[]) {
   int c, chars;
 
   chars = 0;
-  while ((c = getchar()) != '\n' && chars < MAX_INPUT - 1) {
+  while ((c = getchar()) != '\n') {
+    if (chars >= MAX_INPUT - 1) {
+      printf("Maximum input exceeded. Your input will not be saved.\n");
+      free(user_input);
+      exit(1);
+    }
     user_input[chars] = c;
     chars++;
   }
--- prototypes/test.sh
+++ prototypes/test.sh
@@ -17,13 +17,14 @@ fi
 PROGRAM=$1
 
 check_status() {
-    EXIT_CODE=$1
-    INPUT=$2
+    EXIT_CODE=$?
+    INPUT=$@
     if [ $EXIT_CODE -ne 0 ]
     then
-	echo "fixed-width-input exited with failure."
+	echo "$PROGRAM exited with failure."
 	echo "Input was:"
 	echo $INPUT
+	echo "Exit status was $EXIT_CODE"
 	exit 1
     fi
 }
@@ -33,37 +34,44 @@ check_status() {
 
 # Test for short input
 TEST_ONE="This is some text"
+# Test for standard size input
 TEST_TWO="This is some text that is designed to exceed the limit of 50 characters."
+# Test for long input. Should be handled correctly.
 TEST_THREE="This is some text that is designed to exceed the limit of 100 characters with a lot of input text which just keeps going."
+# Test for longest acceptable input
 TEST_FOUR="This is a lot of input text, designed to test the program's ability to handle input that exceeds 150 characters. The output returned should wrap over 4 lines."
+# Test for input with only one space
 ONE_SPACE_TEST="hhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+# Test for input with no spaces. One character of input will be lost,
+# but this is not considered to be a problem, as this is an edge case.
+# No word is longer than 50 characters, which is the recommended minimum.
 NO_SPACES_TEST="hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
 NO_SPACES_LONG_TEST="hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
 MEMORY_LIMIT_TEST="This input is designed to exceed the limit of the memory allocated for the program - currently 200 characters. A large amount of input will be provided to ensure that the program aborts safely. This input should be pasted while the program runs in Valgrind as well, to check for memory leaks."
 
 # Test each input against the program
 echo $TEST_ONE | ./$PROGRAM
-check_status $? $TEST_ONE
+check_status $TEST_ONE
 
 echo $TEST_TWO | ./$PROGRAM
-check_status $? $TEST_TWO
+check_status $TEST_TWO
 
 echo $TEST_THREE | ./$PROGRAM
-check_status $? $TEST_THREE
+check_status $TEST_THREE
 
 echo $TEST_FOUR | ./$PROGRAM
-check_status $? $TEST_FOUR
+check_status $TEST_FOUR
 
 echo $ONE_SPACE_TEST | ./$PROGRAM
-check_status $? $ONE_SPACE_TEST
+check_status $ONE_SPACE_TEST
 
 echo $NO_SPACES_TEST | ./$PROGRAM
-check_status $? $NO_SPACES_TEST
+check_status $NO_SPACES_TEST
 
 echo $NO_SPACES_LONG_TEST | ./$PROGRAM
-check_status $? $NO_SPACES_LONG_TEST
+check_status $NO_SPACES_LONG_TEST
 
 echo $MEMORY_LIMIT_TEST | ./$PROGRAM
-check_status $? $MEMORY_LIMIT_TEST
+check_status $MEMORY_LIMIT_TEST
 
 exit 0
