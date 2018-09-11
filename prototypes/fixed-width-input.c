d79f8cb0c8fbf328a2c04ecf09e2a316d6c21235
Dylan Littler <dylan.littler@tutanota.com>
2018-09-11 16:23:26 +1000

    Filled out find_space() function in fixed-width-input.c

--- prototypes/fixed-width-input.c
+++ prototypes/fixed-width-input.c
@@ -9,3 +9,22 @@
  */
 
 #define MAX_LINE_LENGTH 50
+
+int find_space(char *input, int start, int end) {
+  /* Search for spaces in input before end
+   * and return the number of the index. */
+  int i = 0;
+  for (i = start; i < end; i++) {
+    if (input[MAX_LINE_LENGTH - i] == ' ')
+      return MAX_LINE_LENGTH - i;
+  }
+  return MAX_LINE_LENGTH; // fallthrough in case string contains no spaces
+}
+
+char *insert_newlines(char *input) {
+
+
+  return input;
+}
+
+int main(int argc, char *argv[]);
