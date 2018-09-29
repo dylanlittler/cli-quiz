9b17a3d2dd621b0a8d0f4727e58d68d656e870d8
Dylan Littler <dylan.littler@tutanota.com>
2018-09-29 22:09:38 +1000

    Fix memory bugs in dstring_main.c
    dstring_main now functions without memory leaks. Reallocation of memory takes
    place within main loop, rather than in an external function call.
    The function call did not handle addresses properly, causing memory leaks
    and invalid frees. This must be investigated further.
    dstring.c and dstring.h were deleted, as these are not being used to
    handle reallocation.

--- c/prototypes/dstring_main.c
+++ c/prototypes/dstring_main.c
@@ -1,4 +1,3 @@
-#include "dstring.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,7 +11,7 @@ int main() {
   enable_raw_mode();
   int string_size = 20;
   char *test_string = malloc(string_size);
-
+  //printf("Address at start of program: %p\n", &test_string);
   int c, chars = 0;
 
   while ((c = getchar()) != '\n') {
@@ -21,13 +20,14 @@ int main() {
 
     if (chars >= string_size) {
       string_size += 20;
-      Expand_String(test_string, string_size);
+      test_string = realloc(test_string, string_size);
+      //Expand_String(test_string, string_size);
+      //printf("Address after calling function: %p\n", &test_string);
     }
-
   }
-
+  test_string[chars] = '\0';
   printf("Your input: %s\nChars: %d\n", test_string, chars);
 
-  free(test_string);
+  free(test_string); // invalid free here
   return 0;
 }
